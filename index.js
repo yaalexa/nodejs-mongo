222847b454fc4d225356e43bd08744f73c8555b4
yaalexa <yaalexa25@gmail.com>
2023-09-06 10:46:31 -0500

    nuevo commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const config = require("./src/config/config.js");
-const rutaslibro=require('./src/router/router.js')
+const rutaslibro = require('./src/router/router.js');
+const port = process.env.PORT || 5000 ||config.server.port
 app.use(express.json());
 app.use("/api", rutaslibro);
-app.listen(config.server.port,() => {
+app.listen(port,() => {
     console.log("servidor arriba")
 })
